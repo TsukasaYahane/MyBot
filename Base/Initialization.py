0d6facc4d0974b2e86dce021f06ed57bc8c1b981
TsukasaYahane <TsukasaYahane@outlook.com>
2023-12-11 18:22:20 +0800

    新增初始化内容

--- Base/Initialization.py
+++ Base/Initialization.py
@@ -2,17 +2,35 @@ import requests
 import json
 import http.client
 
-url = "http://127.0.0.1:8088/v1/LuaApiCaller?funcname=MagicCgiCmd&timeout=10&qq=1411365785"
+def ApiCaller() :
+    url = "http://127.0.0.1:8088/v1/LuaApiCaller?funcname=MagicCgiCmd&timeout=10&qq=1411365785"
 
-payload = json.dumps({
-   "CgiCmd": "ClusterInfo",
-   "CgiRequest": {}
-})
-headers = {
+    payload = json.dumps({
+       "CgiCmd": "ClusterInfo",
+       "CgiRequest": {}
+    })
+    headers = {
    'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
    'Content-Type': 'application/json'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.text)
+    print(response.text)
+
+def Friend_Get() :
+    conn = http.client.HTTPSConnection("v1")
+    payload = json.dumps({
+       "CgiCmd": "GetFriendLists",
+       "CgiRequest": {
+          "LastUin": 0
+       }
+    })
+    headers = {
+   'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
+   'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/LuaApiCaller?funcname=MagicCgiCmd&timeout=10&qq=%7B%7BQQBotUid%7D%7D", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
